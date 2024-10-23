bdc4d16335248ef79aaa6e52bb0d7b3cfc79f921
AbhiGithub101 <88555629+AbhiGithub101@users.noreply.github.com>
2024-10-23 17:08:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ if search:
     # Optional: Use regex to clean/validate/format the search query
     match = re.match(r"^(.*?)\s*\(", search)
     movie_title = match.group(1) if match else search  # Use the cleaned title if regex matched, otherwise use the raw search
-    
+
     # Parameterized query to prevent SQL injection
     query = text("SELECT * FROM dbo.movies WHERE title = :title")
 
@@ -48,52 +48,10 @@ if search:
                     st.image(link,width=200)
                 with col2:
                     st.write(wikipedia.summary(movie_title))
-                
+                query = text("SELECT * FROM dbo.reviews WHERE title = :title")
+                df = pd.read_sql_query(query, conn, params={"title": movie_title})
                 st.write("### Movies Data", df)
             else:
                 st.warning("No matching movies found.")
     except Exception as e:
         st.error(f"Error querying the database: {e}")
-
-
-
-
-# # Query data from the 'movies' table
-# import streamlit as st
-# import pandas as pd
-# from sqlalchemy import create_engine, text
-# import urllib
-
-# # Set up SQL Server connection
-# server = 'movieservers.database.windows.net'
-# database = 'moviedatabase'
-# username = 'movielogin'
-# password = 'Arbre@123#'
-# driver = 'ODBC Driver 17 for SQL Server'
-
-# # Create SQLAlchemy connection string
-# connection_string = (
-#     f"mssql+pyodbc://{username}:{urllib.parse.quote_plus(password)}@{server}:1433/"
-#     f"{database}?driver={urllib.parse.quote_plus(driver)}&encrypt=yes&trustServerCertificate=no"
-# )
-# engine = create_engine(connection_string)
-
-# # Streamlit app to search movies
-# search = st.text_input('Search Movie Name:')
-
-# if search:
-#     # Parameterized query to prevent SQL injection
-#     query = text("SELECT * FROM dbo.movies WHERE title = :title")
-
-#     try:
-#         with engine.connect() as conn:
-#             # Execute the query with the user-provided title as a parameter
-#             df = pd.read_sql_query(query, conn, params={"title": search})
-
-#             # Display the results
-#             if not df.empty:
-#                 st.write("### Movies Data", df)
-#             else:
-#                 st.warning("No matching movies found.")
-#     except Exception as e:
-#         st.error(f"Error querying the database: {e}")
